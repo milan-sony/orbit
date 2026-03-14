7c0d1b1ed48622a4bee1db652da89ec5aed5db8a
Milan Sony <milansonyofficial@gmail.com>
2026-03-14 20:55:31 +0530

    chore: return response.data

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,5 +2,5 @@ import axiosInstance from "./axiosConfig";
 
 export async function userSignup(userData) {
     const response = await axiosInstance.post("/auth/signup", userData)
-    return response
+    return response.data
 }
