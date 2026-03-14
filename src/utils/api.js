cc41ba5a70d86851b32715d8ad07ea2740a7bc02
Milan Sony <milansonyofficial@gmail.com>
2026-03-14 20:15:17 +0530

    feat: api's page

---
+++ src/utils/api.js
@@ -0,0 +1,6 @@
+import axiosInstance from "./axiosConfig";
+
+export async function userSignup(userData) {
+    const response = await axiosInstance.post("/auth/signup", userData)
+    return response
+}
