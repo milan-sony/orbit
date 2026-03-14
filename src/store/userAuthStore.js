a7be283887d616d09a98b7a1773cfa71f1bf277e
Milan Sony <milansonyofficial@gmail.com>
2026-03-15 01:16:10 +0530

    feat: user signup data added to store, error handled

--- src/store/userAuthStore.js
+++ src/store/userAuthStore.js
@@ -1,5 +1,5 @@
 import { create } from "zustand";
-import { userSignup } from "../utils/api";
+import { userLogin, userSignup } from "../utils/api";
 
 export const userAuthStore = create((set) => ({
 
@@ -10,16 +10,44 @@ export const userAuthStore = create((set) => ({
     isSigningUp: false,
     isLoggingIn: false,
 
-    signup: async (userData) => {
+    signup: async (userSignupData, navigate) => {
         set({ isSigningUp: true })
         try {
-            const response = await userSignup(userData)
-            console.log("User signup response: ", response)
+            const signupResponse = await userSignup(userSignupData)
+            console.log("Signup response: ", signupResponse)
+            if (signupResponse?.status === 201) {
+                // Create login parameters for skipping the login page in signing up
+                const userLoginData = {
+                    email: userSignupData.email,
+                    password: userSignupData.password
+                }
+
+                console.log("userLoginData from signup: ", userLoginData)
+
+                const loginResponse = await userLogin(userLoginData)
+                console.log("Login response from signup: ", loginResponse)
+
+                if (loginResponse?.status === 200) {
+                    set({
+                        user: loginResponse?.data,
+                        accessToken: loginResponse?.token,
+                        isUserAuthenticated: true
+                    })
+                    alert(loginResponse?.message)
+                    navigate("/home")
+                    return
+                } else {
+                    alert(loginResponse?.message)
+                    return
+                }
+            } else {
+                alert(signupResponse?.message)
+                return
+            }
         } catch (error) {
             console.error("Error signing up the user, ", error)
         } finally {
             set({ isSigningUp: false })
         }
     }
-
 }))
