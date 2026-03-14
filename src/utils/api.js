decd58c7cc78e55fb18e0cd677033139631a1735
Milan Sony <milansonyofficial@gmail.com>
2026-03-15 01:17:12 +0530

    fix: try catch added, response.data is passed in return

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,21 @@
 import axiosInstance from "./axiosConfig";
 
-export async function userSignup(userData) {
-    const response = await axiosInstance.post("/auth/signup", userData)
-    return response.data
+// User signup
+export async function userSignup(userSignupData) {
+    try {
+        const response = await axiosInstance.post("/auth/signup", userSignupData)
+        return response.data
+    } catch (error) {
+        return error?.response?.data
+    }
+}
+
+// User login
+export async function userLogin(userLoginData) {
+    try {
+        const response = await axiosInstance.post("/auth/login", userLoginData)
+        return response.data
+    } catch (error) {
+        return error?.response?.data
+    }
 }
