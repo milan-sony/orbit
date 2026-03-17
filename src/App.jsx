311a61f9a165c8133af6f4796186acde5f6cee7d
Milan Sony <milansonyofficial@gmail.com>
2026-03-17 17:32:03 +0530

    style: preloader added to the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Router from './router/Router'
 import { ThemeProvider } from "./components/theme-provider"
+import PreLoader from './components/PreLoader';
 
 function App() {
+
+  const [isLoading, setIsLoading] = useState(true); // Loading state
+  useEffect(() => {
+    window.addEventListener("load", () => {
+      setIsLoading(false);
+    });
+    const timeout = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
   return (
     <>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <Router />
+        {isLoading ? <PreLoader /> : <Router />}
       </ThemeProvider>
     </>
   )
 }
 
 export default App
-
