6ddb7c9563efeb8418564a0af6b0ea1d15eb16a3
Milan Sony <milansonyofficial@gmail.com>
2026-03-17 19:40:26 +0530

    feat: route protected

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,20 @@
+import { useNavigate } from "react-router-dom";
 import Navbar from "../../components/Navbar";
 import { userAuthStore } from "../../store/userAuthStore";
 
 function Home() {
 
-    const { isUserAuthenticated, accessToken, user } = userAuthStore()
+    const { isUserAuthenticated, accessToken, user, logout } = userAuthStore()
+
+    const navigate = useNavigate()
 
     console.log("accessToken: ", accessToken)
     console.log("isUserAuthenticated: ", isUserAuthenticated)
+    console.log("User: ", user)
+
+    const handleLogout = () => {
+        logout(navigate)
+    }
 
     return (
         <>
@@ -16,6 +24,8 @@ function Home() {
             <p>UserID: {user?.userId}</p>
             <p>Token: {accessToken}</p>
             <p>isUserAuthenticated: {isUserAuthenticated}</p>
+            <button onClick={handleLogout}>Logout</button>
+
         </>
     );
 }
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -5,6 +5,7 @@ import Home from '../pages/Home/Home'
 import PageNotFound from '../pages/PageNotFound/PageNotFound'
 import Signup from '../pages/Signup/Signup'
 import Login from '../pages/Login/Login'
+import ProtectedRoute from '../utils/ProtectedRoute'
 
 function Router() {
     return (
@@ -12,7 +13,7 @@ function Router() {
             <Route path='/' element={<Index />} />
             <Route path='/signup' element={<Signup />} />
             <Route path='/login' element={<Login />} />
-            <Route path='/home' element={<Home />} />
+            <Route path='/home' element={<ProtectedRoute><Home /></ProtectedRoute>} />
             <Route path='*' element={<PageNotFound />} />
         </Routes>
     )
