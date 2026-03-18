939fdb9db97cfe4d8006adb196c9b017456cce89
Milan Sony <milansonyofficial@gmail.com>
2026-03-18 22:56:08 +0530

    style: made homepage responsive

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,31 +1,57 @@
-import { useNavigate } from "react-router-dom";
-import Navbar from "../../components/Navbar";
+import { Button } from "@/components/ui/button";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
 import { userAuthStore } from "../../store/userAuthStore";
+import { menuItems } from "../../utils/constants";
+import { Link } from "react-router-dom";
 
 function Home() {
-
-    const { isUserAuthenticated, accessToken, user, logout } = userAuthStore()
-
-    const navigate = useNavigate()
-
-    console.log("accessToken: ", accessToken)
-    console.log("isUserAuthenticated: ", isUserAuthenticated)
-    console.log("User: ", user)
-
-    const handleLogout = () => {
-        logout(navigate)
-    }
+    const { user } = userAuthStore();
 
     return (
         <>
-            <Navbar />
-            <p>Hello World!</p>
-            <p>User: {JSON.stringify(user)}</p>
-            <p>UserID: {user?.userId}</p>
-            <p>Token: {accessToken}</p>
-            <p>isUserAuthenticated: {isUserAuthenticated}</p>
-            <button onClick={handleLogout}>Logout</button>
+            <div className="p-4 sm:p-6 lg:p-8 space-y-6">
 
+                {/* Greeting */}
+                <h1 className="text-xl sm:text-2xl font-semibold">
+                    Hi {user?.firstName} 👋🏻
+                </h1>
+
+                {/* Grid */}
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+
+                    {menuItems.map((item, index) => (
+                        <Card key={index} className="w-full">
+                            <CardHeader>
+                                <CardTitle>{item.name}</CardTitle>
+                                <CardDescription>
+                                    {item.smallDesc}
+                                </CardDescription>
+                            </CardHeader>
+
+                            <CardContent>
+                                <p>
+                                    {item.desc}
+                                </p>
+                            </CardContent>
+
+                            <CardFooter>
+                                <Button size="sm" className="w-full" asChild>
+                                    <Link to={item.to}>
+                                        Set {item.name}
+                                    </Link>
+                                </Button>
+                            </CardFooter>
+                        </Card>
+                    ))}
+                </div>
+            </div>
         </>
     );
 }
