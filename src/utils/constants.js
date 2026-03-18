df8a65e880ad8fe67459febe7a5ee6c846462a53
Milan Sony <milansonyofficial@gmail.com>
2026-03-18 22:57:19 +0530

    chore: constants

---
+++ src/utils/constants.js
@@ -0,0 +1,33 @@
+// Navbar menus
+export const navbarLinks = [
+    { name: "Home", to: "/home" },
+    { name: "Profile", to: "/profile" },
+];
+
+// Home menu items
+export const menuItems = [
+    {
+        name: "Tasks (To-Do)",
+        to: "/tasks",
+        smallDesc: 'Task = "I need to do this"',
+        desc: "A task is something you need to DO."
+    },
+    {
+        name: "Notes",
+        to: "/notes",
+        smallDesc: 'Note = "I want to remember this"',
+        desc: "A note is information you want to SAVE."
+    },
+    {
+        name: "Remainder",
+        to: "/remainders",
+        smallDesc: 'Reminder = "Notify me at this time"',
+        desc: "A reminder is a trigger at a specific time."
+    },
+    // {
+    //     name: "Tracks",
+    //     to: "/tracks",
+    //     smallDesc: 'Task = "I need to do this"',
+    //     desc: "A task is something you need to DO (action)."
+    // },
+];
