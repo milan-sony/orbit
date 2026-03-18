704eec8ceeb6b1dd4f134b58b7c333d1775461fd
Milan Sony <milansonyofficial@gmail.com>
2026-03-18 22:56:27 +0530

    chore: page created

---
+++ src/pages/Remainders/Remainders.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Remainders() {
+    return (
+        <div>
+            <h1>Remainders page</h1>
+        </div>
+    )
+}
+
+export default Remainders
---
+++ src/pages/Tasks/Tasks.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Tasks() {
+    return (
+        <div>
+            <h1>Tasks page</h1>
+        </div>
+    )
+}
+
+export default Tasks
